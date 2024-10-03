b192e932c9d8e96323982ae64acef1cb90e40a04
DoC-Daniel <daniel.ching@docmapping.com>
2024-10-03 10:53:05 -0700

    add

---
+++ src/Home.tsx
@@ -0,0 +1,15 @@
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="title">
+        Ticket to Ride
+      </div>
+      <hr></hr>
+      <button>
+        Play
+      </button>
+    </div>
+  )
+}
+
+export default Home
